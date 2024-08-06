d78d23f7a734b4d2e58cf75014afdece0823b015
jessicalealR <jessikrojaz99@gmail.com>
2024-08-05 21:40:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
